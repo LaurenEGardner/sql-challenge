b1fe413f467bbbcb2d7ec2a057b737e43f0582c0
LaurenEGardner <heylo.gardner@gmail.com>
2021-03-12 11:36:54 -0800

    made data analysis questions easier to read in the window

--- EmployeeSQL/dataAnalysis.sql
+++ EmployeeSQL/dataAnalysis.sql
@@ -11,14 +11,20 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
--- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+-- List the manager of each department with the following information:
+-- department number, department name, the manager's employee number, last name, first name.
 
--- List the department of each employee with the following information: employee number, last name, first name, and department name.
+-- List the department of each employee with the following information:
+-- employee number, last name, first name, and department name.
 
--- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+-- List first name, last name, and sex for employees whose first name is
+--  "Hercules" and last names begin with "B."
 
--- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+-- List all employees in the Sales department, including their 
+--  employee number, last name, first name, and department name.
 
--- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- List all employees in the Sales and Development departments, 
+--  including their employee number, last name, first name, and department name.
 
--- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+-- In descending order, list the frequency count of employee last names, 
+--  i.e., how many employees share each last name.
