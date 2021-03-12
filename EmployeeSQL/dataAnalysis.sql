a3841753ea81cc29ac519f4f7f4c0ba012e6a7ae
LaurenEGardner <heylo.gardner@gmail.com>
2021-03-12 11:41:26 -0800

    finished a few more analysis questions

--- EmployeeSQL/dataAnalysis.sql
+++ EmployeeSQL/dataAnalysis.sql
@@ -19,6 +19,9 @@ WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 -- List first name, last name, and sex for employees whose first name is
 --  "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WEHRE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 -- List all employees in the Sales department, including their 
 --  employee number, last name, first name, and department name.
@@ -28,3 +31,7 @@ WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 -- In descending order, list the frequency count of employee last names, 
 --  i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS name_count
+FROM employees
+GROUP BY last_name
+ORDER BY name_count DESC;
