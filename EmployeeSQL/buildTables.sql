a5dd2cf50e8bc5a8360bbdf7d7cd465222eae854
LaurenEGardner <heylo.gardner@gmail.com>
2021-03-12 11:32:12 -0800

    finished building tables, starting on data analysis

--- EmployeeSQL/buildTables.sql
+++ EmployeeSQL/buildTables.sql
@@ -11,17 +11,55 @@ CREATE TABLE titles (
 -- Create employees table after deleting any previous iterations
 DROP TABLE IF EXISTS employees;
 
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
 
 -- Create salaries table after deleting any previous iterations
 DROP TABLE IF EXISTS salaries;
 
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
 
 -- Create departments table after deleting any previous iterations
 DROP TABLE IF EXISTS departments;
 
+CREATE TABLE departments (
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no)
+);
 
 -- Create dept_emp table after deleting any previous iterations
 DROP TABLE IF EXISTS dept_emp;
 
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no,dept_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
 -- Create dept_manager table after deleting any previous iterations
 DROP TABLE IF EXISTS dept_manager;
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (emp_no,dept_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
