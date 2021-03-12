5e0d97291fe780f1dcb25aa7c3524789af183df2
LaurenEGardner <heylo.gardner@gmail.com>
2021-03-12 12:01:11 -0800

    finished a few more analysis

--- EmployeeSQL/dataAnalysis.sql
+++ EmployeeSQL/dataAnalysis.sql
@@ -13,10 +13,18 @@ WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 -- List the manager of each department with the following information:
 -- department number, department name, the manager's employee number, last name, first name.
-
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager dm
+	JOIN departments d ON dm.dept_no = d.dept_no
+	JOIN employees e on dm.emp_no = e.emp_no;
+	
 -- List the department of each employee with the following information:
 -- employee number, last name, first name, and department name.
-
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp
+	JOIN employees e ON e.emp_no = dept_emp.emp_no
+	JOIN departments d on d.dept_no = dept_emp.dept_no;
+	
 -- List first name, last name, and sex for employees whose first name is
 --  "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex
