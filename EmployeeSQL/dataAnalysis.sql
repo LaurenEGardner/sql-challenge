f185abc9040429c84b746fd0ee798084a2c0443d
LaurenEGardner <heylo.gardner@gmail.com>
2021-03-12 11:35:29 -0800

    finished some analysis

--- EmployeeSQL/dataAnalysis.sql
+++ EmployeeSQL/dataAnalysis.sql
@@ -1,8 +1,15 @@
 -- Data Analysis
 
 -- List the following details of each employee: employee number, last name, first name, sex, and salary.
-
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, e.salary
+FROM employees e
+	JOIN salaries s
+		ON e.emp_no = s.emp_no;
+	
 -- List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
